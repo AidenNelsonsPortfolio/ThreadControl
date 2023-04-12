e41c052fd3f8e12a04f3cfab12feadf216add63c
Aiden Nelson <aidenjnelson1@gmail.com>
2023-04-11 20:38:54 -0500

    Initial commit.

---
+++ project3_353.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Aiden Nelson, EID: 800742353
+Date of Start: 4/11/2023
+
+Project Topic: 
+    This program uses threads (UNIX) to control 5 individual threads with a self-made 
+    interrupt handler/thread scheduler. This project also uses skills in MUTEX
+    semaphores to ensure safe operation. 
+
+    The project runs on an infinite loop (with console statements showing the progress through the program).
+
+    Thanks for trying it out, I hope you enjoy!
+
+*/
+
+
